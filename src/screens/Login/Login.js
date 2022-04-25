392e8e2598c3e3f26cdefb0714a00ec33cd882fe
Samir Perez <MY_NAME@example.com>
2022-04-25 18:52:19 -0500

    feat: created submit function

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,25 +1,45 @@
-import { TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import CustomText from '../../components/CustomText/CustomText'
 import styles from './styles'
 import Input from '../../components/Input/Input'
+import { TouchableOpacity, View } from 'react-native'
+import { useForm } from '../../hooks/useForm'
+import { loginValidation } from '../../helpers/validation'
 
 const Login = () => {
 
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [values, errors, setErrors, handleChange] = useForm({ email: '', password: '' });
+
+
+  const handleSubmit = () => {
+    const loginErrors = loginValidation(values);
+    
+    if (Object.keys(loginErrors).length > 0) {
+      setErrors(loginErrors);
+    } else {
+      setErrors({});
+      //TODO: FETCH API 
+    }
+  }
 
   return (
     <View>
       <CustomText customStyles={styles.title}>Ingresa</CustomText>
       <View style={styles.inputContainer}>
         <CustomText customStyles={styles.inputTitle}>Email</CustomText>
-        <Input placeHolder="Ingrese su correo" value={email} onChange={setEmail} />
+        <Input placeHolder="Ingrese su correo" name="email" value={values.email} onChange={handleChange} />
         <CustomText customStyles={styles.inputTitle}>Contraseña</CustomText>
-        <Input placeHolder="Ingrese su contraseña" value={password} onChange={setPassword} isPassword/>
+        <Input placeHolder="Ingrese su contraseña" name="password" value={values.password} onChange={handleChange} isPassword />
       </View>
-      <TouchableOpacity 
+
+      {
+        errors &&
+        <CustomText customStyles={styles.errorMessage}>{Object.values(errors)[0]}</CustomText>
+      }
+
+      <TouchableOpacity
         style={styles.button}
+        onPress={handleSubmit}
       >
         <CustomText customStyles={styles.buttonText}>ingresar</CustomText>
       </TouchableOpacity>
