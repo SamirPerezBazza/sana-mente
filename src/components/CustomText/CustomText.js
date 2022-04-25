7db5a068ce67a020a64e75f91db6d065b0358afa
Samir Perez <MY_NAME@example.com>
2022-04-25 14:27:47 -0500

    First Commit

---
+++ src/components/CustomText/CustomText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles  from './styles';
+import { Text } from 'react-native';
+
+const CustomText = ({ children }) => {
+  return <Text>{children}</Text>;
+};
+
+
+export default CustomText;
